67c35f4d3416df3407eeb49b4ea6edbb1be8d03c
abc <abc123@gmail.com>
2026-02-24 17:51:54 +0530

    first commit

--- o1.py
+++ o1.py
@@ -1,8 +1,11 @@
 a=int(input("Enter a number: "))
 b=int(input("Enter another number: "))
-if a>b:
-    print("The first number is greater than the second number.")
-elif a<b:
-    print("The first number is less than the second number.")
+c=int(input("Enter a third number: "))
+if a>b and a>c:
+    print("The first number is greater than the second and third numbers.")
+elif b>a and b>c:
+    print("The second number is greater than the first and third numbers.")
+elif c>a and c>b:
+    print("The third number is greater than the first and second numbers.")
 else:
     print("The first number is equal to the second number.")
