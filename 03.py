56ed1a16aec3e48db95f6e1f9ab80d33abb89dad
abc <abc123@gmail.com>
2026-02-25 17:26:45 +0530

    added by sub1

--- 03.py
+++ 03.py
@@ -1,5 +1,10 @@
 def add():
     a = int(input("Enter first number: "))
     b = int(input("Enter second number: "))
-    return a + b
+    if(a>b):
+        print("a is greater")
+    else:
+        print("b is greater")
+    print(a + b)
+
 print("The sum of the two numbers is:", add())
