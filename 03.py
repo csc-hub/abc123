273accda095580655d8f158fcc0abb0f68f79695
abc <abc123@gmail.com>
2026-02-25 17:29:11 +0530

    added by sub2

--- 03.py
+++ 03.py
@@ -3,3 +3,11 @@ def add():
     b = int(input("Enter second number: "))
     return a + b
 print("The sum of the two numbers is:", add())
+c= int(input("Enter a number: "))
+print("The result after subtraction is:", c)
+if c > 0:
+    print("The number is positive.")
+elif c < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")
